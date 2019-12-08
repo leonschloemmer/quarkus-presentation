6255cb820bc4ac313dda5b303ba9bbc30ecc8aa0
Leon Schlömmer <leon.schloemmer@posteo.at>
2019-12-08 22:17:26 +0100

    implemented toHash method

--- src/main/java/at/htl/control/PasswordSecurity.java
+++ src/main/java/at/htl/control/PasswordSecurity.java
@@ -1,14 +1,26 @@
 package at.htl.control;
 
+import com.fasterxml.jackson.databind.node.BigIntegerNode;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.interfaces.PBEKey;
+import javax.crypto.spec.PBEKeySpec;
+import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
 public class PasswordSecurity {
 
-    public static String generatePasswordHash(String password) throws NoSuchAlgorithmException {
+    public static String generatePasswordHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
         int iterations = 1000;
         char[] chars = password.toCharArray();
         byte[] salt = getSalt();
+
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
     }
 
     private static byte[] getSalt() throws NoSuchAlgorithmException {
@@ -16,6 +28,17 @@ public class PasswordSecurity {
         byte[] salt = new byte[16];
         sr.nextBytes(salt);
         return salt;
+    }
+
+    private static String toHash(byte[] array) throws NoSuchAlgorithmException {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        if (paddingLength > 0) {
+            return String.format("%0" + paddingLength + "d", 0) + hex;
+        } else {
+            return hex;
+        }
     }
 
 }
